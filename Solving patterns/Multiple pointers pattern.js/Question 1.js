3d7383a7d74afe4f155d0997fb49e76235264f38
Apoorv <apoorvsharma494@gmail.com>
2022-06-29 17:03:40 +0530

    commit

--- Solving patterns/Multiple pointers pattern.js/Question 1.js
+++ Solving patterns/Multiple pointers pattern.js/Question 1.js
@@ -4,27 +4,39 @@
 
 //................................naive solution............................
 
-function averagePair(arr, avg) {
- 
-    let avg2 = avg + avg
- 
+// function averagePair(arr, avg) {
 
+//     let avg2 = avg + avg
+
+
+//     if (arr.length == 0) {
+//         console.log(false)
+//     }
+
+//     else {
+//         for (let i = 0; i < arr.length; i++) {
+//             for (let j = 0; j < arr.length; j++) {
+
+//                 if (arr[i] + arr[j] === avg2) {
+//                     console.log(arr[i], arr[j])
+//                 }
+
+//             }
+
+//         }
+//     }
+// }
+
+// averagePair([1,3, 3, 5, 6, 7, 10, 12, 19], 8)
+
+
+
+//.........................two pointer method...................................
+
+function averagePair(arr, avg) {
     if (arr.length == 0) {
         console.log(false)
     }
-
-    else {
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr.length; j++) {
-
-                if (arr[i] + arr[j] === avg2) {
-                    console.log(arr[i], arr[j])
-                }
-
-            }
-
-        }
-    }
 }
 
-averagePair([1,3, 3, 5, 6, 7, 10, 12, 19], 8)
+averagePair([1, 2, 3], 2.5)
