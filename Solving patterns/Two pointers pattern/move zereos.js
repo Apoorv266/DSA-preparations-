949d241ad79447c6fe2a7c51dc6e4bf6915bee1b
Apoorv <apoorvsharma494@gmail.com>
2022-07-06 03:59:28 +0530

    commit

---
+++ Solving patterns/Two pointers pattern/move zereos.js
@@ -0,0 +1,6 @@
+// give an array nums, write a function to move all 0s to the end of it while maintaining the relative order of the non-zero elements
+
+// input -> nums = [0, 1, 0 , 3, 12]
+// output -> [1, 3, 12, 0 , 0]
+
+
