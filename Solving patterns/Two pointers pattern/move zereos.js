1f4995284683f981626a2645abe21149d68507ad
Apoorv <apoorvsharma494@gmail.com>
2022-07-06 22:22:49 +0530

    commit

--- Solving patterns/Two pointers pattern/move zereos.js
+++ Solving patterns/Two pointers pattern/move zereos.js
@@ -4,3 +4,24 @@
 // output -> [1, 3, 12, 0 , 0]
 
 
+function moveZero(nums) {
+    // let arr = arr2.sort(function (a, b) { return a - b }).reverse()
+    let start = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[start] = nums[i]
+            start++
+        }
+    }
+
+    console.log(nums)
+
+    for (let i = start; i < nums.length; i++) {
+        nums[i] = 0
+    }
+
+    console.log(nums)
+}
+
+moveZero([0,1,0,3,12])
