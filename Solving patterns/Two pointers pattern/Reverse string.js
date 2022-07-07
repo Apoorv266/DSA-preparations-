b32085f4a6cea64205bcde0d1ebfc372f7156e93
Apoorv <apoorvsharma494@gmail.com>
2022-07-07 17:04:30 +0530

    commit

--- Solving patterns/Two pointers pattern/Reverse string.js
+++ Solving patterns/Two pointers pattern/Reverse string.js
@@ -2,5 +2,32 @@
 
 
 var reverseString = function(s) {
-    
+    console.log(s)
+   let start = 0;
+   let end = s.length - 1
+ 
+   while (start < end) {
+// swapping last element with first
+    let temp = s[end]
+    s[end] = s[start]
+    s[start] = temp
+
+    start++
+    end--
+   }
+
+     console.log(s)
 };
+
+
+reverseString(["H","a","n","n","a","h"])
+
+// swap 2 numbers
+
+// let a = 1
+// let b = 2
+// let temp = a
+// a = b
+// b = temp
+
+// console.log("b", b , "a" , a) 
