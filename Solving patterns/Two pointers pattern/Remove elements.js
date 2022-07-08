ce2482e7e80c6cdabb9f4719e44e7ef81017973b
Apoorv <apoorvsharma494@gmail.com>
2022-07-08 23:32:36 +0530

    commit

---
+++ Solving patterns/Two pointers pattern/Remove elements.js
@@ -0,0 +1,22 @@
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+
+
+var removeElement = function (nums, val) { 
+    console.log(nums)
+
+    let start = 0 
+
+    while (start < nums.length) {
+        if (nums[start] === val) {
+            nums[start].pop()
+        }
+
+        start++
+    }
+
+    console.log(nums)
+};
+
+
+removeElement([3, 2, 2, 3], 3)
