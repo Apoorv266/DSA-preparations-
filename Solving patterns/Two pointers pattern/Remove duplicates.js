21720e415f57af0cbc01836e571bd489cabb7823
Apoorv <apoorvsharma494@gmail.com>
2022-08-05 20:14:56 +0530

    commit

--- Solving patterns/Two pointers pattern/Remove duplicates.js
+++ Solving patterns/Two pointers pattern/Remove duplicates.js
@@ -1,5 +1,12 @@
 var removeDuplicates = function(nums) {
+//    let start = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i + 1] ) {
+            nums[i] = nums[i + 1]
+            // start++
+        }
+    }
     return nums
 };
 
-console.log(removeDuplicates([1, 1, 2, 3, 4]))
+console.log(removeDuplicates( [0,0,1,1,1,2,2,3,3,4]))
