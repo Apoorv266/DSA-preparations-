399e516d32de6cb4d26cc2fc981ea7399f6d214f
Apoorv <apoorvsharma494@gmail.com>
2022-08-05 13:41:20 +0530

    commit

--- arrays/easy/Rotate array.js
+++ arrays/easy/Rotate array.js
@@ -1,1 +1,24 @@
 // rotate an array to left by k steps
+function rotate(arr , k){
+
+    
+for (let j = 0; j < k; j++) {
+   
+    for (let i = 0; i < arr.length; i++) {
+       if (i === 0) {
+         arr.push(arr[i])
+       } 
+    }
+    arr.shift()
+}
+
+return arr
+
+}
+
+console.log(rotate([1,3,6, 11, 12, 17],1))
+
+
+
+
+
