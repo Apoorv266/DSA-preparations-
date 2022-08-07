5782f3f62fa9251e6357c6f940df175762ba330d
Apoorv <apoorvsharma494@gmail.com>
2022-08-07 11:37:02 +0530

    commit

--- arrays/easy/Add array.js
+++ arrays/easy/Add array.js
@@ -1,20 +1,48 @@
 function addArr(arr1, arr2) {
     let i = arr1.length - 1
-    let j = arr2.length - 1
+    let j =  arr2.length - 1
     let ans = []
+    let carry = 0
 
     while (i >= 0 && j >= 0) {
-        let sum = arr1[i] + arr2[j]
-        ans.push[sum]
+        let val1 = arr1[i];
+        let val2 = arr2[j];
+       let sum = val1 + val2 + carry;  
+        carry = parseInt(sum / 10)
+        sum = sum % 10
+        ans.push(sum)
         i--
         j--
     }
 
-    return ans
+    while (i >= 0) {
+        let sum = arr1[i] + carry;  
+        carry = parseInt(sum / 10)
+        sum = sum % 10
+        ans.push(sum)
+        i--
+    }
+
+    while (j >= 0) {
+        let sum = arr2[j] + carry;  
+        carry = parseInt(sum / 10)
+        sum = sum % 10
+        ans.push(sum)
+        j--
+    }
+
+    while(carry != 0) {
+        let sum = carry;
+        carry = parseInt(sum/10);
+        sum = sum%10;
+        ans.push(sum);
+    }
+    return ans.reverse()
 }
 
-console.log(addArr([1, 1, 1],[1, 1, 1]))
+console.log(addArr([9, 9],[9, 9, 9]))
 
-// let ar = [1, 2, 3]
-// let n = ar.length - 1
-// console.log(ar[n])
+let a = 1
+// let b = 1
+// let c = a + b
+console.log(parseInt(a / 10))
