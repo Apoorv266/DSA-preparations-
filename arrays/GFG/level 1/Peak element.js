b3a1a25009b792965a82fb6adc2b75f484ace405
Apoorv <apoorvsharma494@gmail.com>
2022-08-09 20:23:16 +0530

    commit

--- arrays/GFG/level 1/Peak element.js
+++ arrays/GFG/level 1/Peak element.js
@@ -1,14 +1,19 @@
-function peakElement(arr, n){
-for (let i = 0; i < n; i++) {
-    if (arr[i] >= arr[i - 1] && arr[i + 1] >= arr[i]) {
-        return arr[i]
-    }
+function peakElement(arr, n) {
+    for (let i = 0; i < n; i++) {
+        if (arr[i - 1] <= arr[i] && arr[i + 1] <= arr[i]) {
+            return arr[i]
+        }
 
-    else if (arr[i] >= arr[i - 1] && !arr[i + 1]) {
-       console.log(arr[i])
+        else if ( arr[i - 1] <= arr[i]  && typeof arr[i + 1] == undefined) {
+            console.log(arr[i])
+        }
+
+        else if (arr[i] >= arr[i + 1] && typeof arr[i - 1] == undefined) {
+            console.log(arr[i])
+        }
     }
-    
-}
 }
 
+let arr2 = [1, 2, 3]
 console.log(peakElement([1, 2, 3], 3))
+console.log(arr2[1 + 1])
