747f92820839123fcd82b48eff1372c15110b3d9
Apoorv <apoorvsharma494@gmail.com>
2022-08-10 18:08:16 +0530

    commit

--- arrays/GFG/level 1/Sort The Array.js
+++ arrays/GFG/level 1/Sort The Array.js
@@ -1,21 +1,40 @@
 // Given a random set of numbers, Print them in sorted order.
 
+// function sortArr(n, arr) {
+//     var done = false
+//     while (!done) {
+//         done = true;
+//         for (let i = 0; i < n; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 done = false
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+
 function sortArr(n, arr) {
-    var  done = false
-    while (!done) {
-        done = true;
-    for (let i = 1; i < n; i++) {
-        if (arr[i - 1] > arr[i]) {
-            done = false
-           let temp = arr[i - 1]
-            arr[i - 1] = arr[i]
-            arr[i] = temp;
-        }
+    for (let i = 0; i < n; i++) {
+        if (Math.sign(arr[i] - arr[i + 1]) == 1) {
+           let temp = arr[i + 1]
+           arr[i + 1] = arr[i]
+           arr[i] = temp
+        } 
     }
-}
     return arr
 }
-console.log(sortArr(4, [5, 4, 8, 3]))
+
+console.log(sortArr(6, [5, 4, 1, 2]))
+
+
+let a = 5
+let b = 4
+
+console.log(Math.sign(a - b))
 
 
 
