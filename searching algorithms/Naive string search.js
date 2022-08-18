ae62d6942680d18351ce99c727c775b8ba407836
Apoorv <apoorvsharma494@gmail.com>
2022-08-18 11:54:26 +0530

    commit

--- searching algorithms/Naive string search.js
+++ searching algorithms/Naive string search.js
@@ -1,5 +1,17 @@
 function stringSearch(str1, str2) {
-    
+    let count = 0
+    for (let i = 0; i < str1.length; i++) {
+        for (let j = 0; j < str2.length; j++) {
+            if (str2[j] !== str1[i + j]) {
+                break
+            }
+            // if the shorter string is checked completely then it means all the string items matches in the bigger string
+           if (j === str2.length - 1) {
+             count++
+           }
+        }
+    }
+    return count
 }
 
-stringSearch(abcdef, bc)
+console.log(stringSearch("wowomgzomg", "omg"))
