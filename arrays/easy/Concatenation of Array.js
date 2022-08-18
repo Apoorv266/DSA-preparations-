2e774151f2f947bcbc3414237d9a561aa5d7d17f
Apoorv <apoorvsharma494@gmail.com>
2022-08-18 12:27:00 +0530

    commit

--- arrays/easy/Concatenation of Array.js
+++ arrays/easy/Concatenation of Array.js
@@ -1,13 +1,16 @@
 // Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i]
 
-var getConcatenation = function(nums) {
-    let ans =[]
-    for (let j = 0; j < 2; j++) {
+var getConcatenation = function (nums) {
+    let ans = []
+    let j = 0
+    while (j < 2) {
         for (let i = 0; i < nums.length; i++) {
             ans.push(nums[i])
         }
+        j++
     }
     return ans
 };
 
-console.log(getConcatenation([1,3,2,1]))
+
+console.log(getConcatenation([1, 3, 2, 1]))
