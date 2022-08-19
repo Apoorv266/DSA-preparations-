8bd51e8ddf1e3261c5aa918acdef950d22f499c7
Apoorv <apoorvsharma494@gmail.com>
2022-08-20 03:52:32 +0530

    commit

--- arrays/easy/Watering Plants.js
+++ arrays/easy/Watering Plants.js
@@ -1,27 +1,36 @@
 var wateringPlants = function (plants, capacity) {
 
     let counter = 0
+    // store the capacity in val since we have to modify the capacity
     let val = capacity
-    
+
+    // loop over the plants array
     for (let i = 0; i < plants.length; i++) {
-        
+
+        // if val is higher than current value then substract it from current value and update the val
+        // val is updated for next loop
+        // increase counter by 1
         if (val >= plants[i]) {
             val = val - plants[i]
-            counter+=1
+            counter += 1
         }
-        
+
+        // if the val is lower than the currect value then fill up the val with capacity again and then then substract it from current value and update the val
+         // val is updated for next loop
         else if (val <= plants[i]) {
             val = capacity
             val = val - plants[i]
+            // increase the counter by number of steps we need to go back to river from current element (i steps) and number of steps we need to come back again to current element (i + 1 steps)
             counter += i + (i + 1)
         }
     }
-    
+
+    // return counter
     return counter
 };
 
 
-console.log(wateringPlants([3,2,4,2,1], 6))
+console.log(wateringPlants([1, 1, 1, 4, 2, 3], 4))
 
 
 // let a = 2
