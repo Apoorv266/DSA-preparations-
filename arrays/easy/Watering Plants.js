42d9f6cf40f64e6cf978cebe1715a32fe373ddeb
Apoorv <apoorvsharma494@gmail.com>
2022-08-20 02:04:43 +0530

    commit

--- arrays/easy/Watering Plants.js
+++ arrays/easy/Watering Plants.js
@@ -1,11 +1,25 @@
-var wateringPlants = function(plants, capacity) {
-    console.log(plants[plants.length - 1])
+var wateringPlants = function (plants, capacity) {
+    // console.log(plants[plants.length - 1])
     let counter = 0
+    let val = capacity
 
     for (let i = 0; i < plants.length; i++) {
+        let subs = val - plants[i]
+
+        if (val > plants[i]) {
+            val = subs
+            counter+=1
+        }
         
-        
+        else if (val < plants[i]) {
+            val = capacity
+            let add = 2 + i
+            counter +=add 
+        }
+        console.log(subs,val,counter)
     }
+    
+    return counter
 };
 
-console.log(wateringPlants([2,2,3,3],5))
+console.log(wateringPlants([2, 2, 3, 3], 5))
