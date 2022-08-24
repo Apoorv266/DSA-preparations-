29bbd68213c30244a91eed8244540125fe143ce7
Apoorv <apoorvsharma494@gmail.com>
2022-08-24 22:03:37 +0530

    commit

--- arrays/easy/nearest point having same X or Y.js
+++ arrays/easy/nearest point having same X or Y.js
@@ -1,11 +1,13 @@
 var nearestValidPoint = function (points, x, y) {
     let val = Infinity
     let index = Infinity
+
     for (let i = 0; i < points.length; i++) {
         for (let j = 0; j < points[i].length - 1; j++) {
             if (points[i][j] === x || points[i][j + 1] === y) {
-                let a =  Math.abs(points[i][j] - x) + Math.abs(points[i][j + 1] - y)
-                if (a < val && i < index) {
+                let a = Math.abs(points[i][j] - x) + Math.abs(points[i][j + 1] - y)
+                if (a <= val) {
+                    console.log(a)
                     val = a
                     index = i
                     console.log(points[i])
@@ -15,7 +17,6 @@ var nearestValidPoint = function (points, x, y) {
     }
     // return arr
 };
-
 
 console.log(nearestValidPoint([[1, 2], [3, 1], [2, 4], [2, 3], [4, 4]], 3, 4))
 
