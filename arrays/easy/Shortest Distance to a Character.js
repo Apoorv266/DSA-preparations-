d6647ca4d74491104981576192cc0f9b173cd1ba
Apoorv <apoorvsharma494@gmail.com>
2022-08-24 12:06:38 +0530

    Commit

--- arrays/easy/Shortest Distance to a Character.js
+++ arrays/easy/Shortest Distance to a Character.js
@@ -2,7 +2,7 @@ var shortestToChar = function (s, c) {
     let arr = []
     let arr2 = []
     for (let i = 0; i < s.length; i++) {
-        
+
         if (s[i] === c) {
             arr.push(i)
         }
@@ -25,17 +25,47 @@ var shortestToChar = function (s, c) {
 console.log(shortestToChar("abcdpd", "d"))
 
 
+// second approach 
 
+var shortestToChar = function (s, c) {
+    let output = []
 
-function findsmall(params) {
-    let val = Infinity
-    for (let i = 0; i < params.length; i++) {
-        if (params[i] < val) {
-            val = params[i]
+    for (let i = 0; i < s.length; i++) {
+        let item = findDistance(i, s, c)
+        output.push(item)
+    }
+    return output
+
+    function findDistance(i, s, c) {
+        if (s[i] === c) {
+            return 0
         }
 
+        let j = 1
+        while (j >= 0 || j < s.length) {
+            if (s[i - j] === c || s[i + j] === c) {
+                return j
+            }
+            j++
+        }
     }
-    return val
 }
 
-console.log(findsmall([3, 76, 53, 2]))
+
+
+
+
+// function findsmall(params) {
+//     let val = Infinity
+//     for (let i = 0; i < params.length; i++) {
+//         if (params[i] < val) {
+//             val = params[i]
+//         }
+
+//     }
+//     return val
+// }
+
+// console.log(findsmall([3, 76, 53, 2]))
+
+
