e60186778d27400d4106d07b0aec2e6fcc6ec697
Apoorv <apoorvsharma494@gmail.com>
2022-08-26 14:56:51 +0530

    commit

--- arrays/easy/merge 2 array in alternate manner.js
+++ arrays/easy/merge 2 array in alternate manner.js
@@ -22,14 +22,37 @@ function merge(arr1, arr2) {
 }
 
 
-
-
-let arr1 = [1, 2, 3, 4, 5]
+let arr1 = [1, 2, 3, 4,5, 6, 7]
 let arr2 = [11, 22, 33, 44]
 
 console.log(merge(arr1, arr2))
 
+//-----------or--------------
 
+
+function alternateMerge(arr1, arr2) {
+    let arr3 = []
+    let i = 0, j = 0, k = 0;
+
+    // Traverse both array till equal value
+    while (i < arr1.length && j < arr2.length) {
+        arr3[k++] = arr1[i++];
+        arr3[k++] = arr2[j++];
+    }
+
+    // Store remaining elements of first array
+    while (i < arr1.length)
+        arr3[k++] = arr1[i++];
+
+    // Store remaining elements of second array
+    while (j < arr2.length)
+        arr3[k++] = arr2[j++];
+
+        return arr3
+}
+
+console.log(alternateMerge(arr1, arr2)
+)
 
 
 
