dbe868b12513acb6e38509ddc901ee6f1df81d02
Apoorv <apoorvsharma494@gmail.com>
2022-09-13 05:14:36 +0530

    Commit

--- arrays/easy/Three Consecutive Odds.js
+++ arrays/easy/Three Consecutive Odds.js
@@ -1,13 +1,40 @@
-var threeConsecutiveOdds = function(arr) {
-  
-   for (let i = 1; i <= arr.length - 2; i++) {
-   
-    if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i - 1] % 2 !== 0) {
-        return true
+var threeConsecutiveOdds1 = function (arr) {
+
+    for (let i = 1; i <= arr.length - 2; i++) {
+
+        if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i - 1] % 2 !== 0) {
+            return true
+        }
+
     }
-    
-   }
-   return false
+    return false
 };
 
-console.log(threeConsecutiveOdds([1, 1, 1]))
+console.log(threeConsecutiveOdds1([1, 2, 34, 3, 4, 5, 7, 23, 12]))
+
+
+// or
+
+
+var threeConsecutiveOdds2 = function (arr) {
+
+    let counter = 0
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] % 2 !== 0) {
+            counter++
+        }
+
+        else{
+            counter = 0
+        }
+
+        if(counter === 3){
+            return true
+        }
+
+    }
+    return false
+};
+
+console.log(threeConsecutiveOdds2([1, 3]))
