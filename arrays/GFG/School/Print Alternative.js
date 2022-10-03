41fbe89b2308ba349fc05acbcd36ccf802978777
Apoorv <apoorvsharma494@gmail.com>
2022-10-03 05:30:54 +0530

    commit

--- arrays/GFG/School/Print Alternative.js
+++ arrays/GFG/School/Print Alternative.js
@@ -1,9 +1,10 @@
 function print(arr, n) {
     let str = ""
-    for (let i = 0; i <= arr.length; i = i + 2) {
+    for (let i = 0; i < n ; i = i + 2) {
         str += arr[i]
+        console.log(arr[i])
     }
-    return str
+    return str.split('').join(' ') 
 }
 
-console.log(print([1, 2, 3, 4, 5]))
+console.log(print([32, 41, 23, 3, 1, 6], 6))
