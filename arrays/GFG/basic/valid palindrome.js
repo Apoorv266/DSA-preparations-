c99515fd8c5c15ae0d4b54048f1c2dd8e4b8ada6
Apoorv <apoorvsharma494@gmail.com>
2022-10-26 23:56:13 +0530

    commit

---
+++ arrays/GFG/basic/valid palindrome.js
@@ -0,0 +1,23 @@
+var isPalindrome = function (s) {
+    s = s.toLowerCase().replace(/[^a-z\d]/g, '');
+    // s  = s.replace(/[^a-zA-Z0-9 ]/g, '');
+
+    
+    let start = 0
+    let end = s.length - 1
+
+    if (s.length === 0) {
+        return true
+    }
+    while (start < end) {
+        if (s[start] !== s[end]) {
+            return false
+        }
+            start++
+            end--
+    }
+    return true
+
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"))
